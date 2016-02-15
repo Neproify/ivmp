48a7dd16e60a601617e4944ed2e6daab296f7155
Neproify <yamasaki3214@gmail.com>
2016-02-15 16:24:26 +0100

    I forget about script.img ;)

--- Release Tools/redist_client.bat
+++ Release Tools/redist_client.bat
@@ -5,7 +5,9 @@ SET REVISION=r%%F
 SET VERSION="0.1"
 echo f | xcopy /f "../bin/ScriptHookDotNet.dll" "../bin/ScriptHookDotNet.asi"
 echo f | xcopy /f "../bin/ivmp_client_core.dll" "../bin/scripts/ivmp_client_core.net.dll"
+echo f | xcopy /f "../client/scriptimg/script.img" "../bin/data/common/cdimages/script.img"
 7z a IVMP-%VERSION%-%REVISION%-Client.zip ../bin/scripts
 7z a IVMP-%VERSION%-%REVISION%-Client.zip ../bin/Lidgren.Network.dll
 7z a IVMP-%VERSION%-%REVISION%-Client.zip ../bin/ScriptHook.dll
 7z a IVMP-%VERSION%-%REVISION%-Client.zip ../bin/ScriptHookDotNet.asi
+7z a IVMP-%VERSION%-%REVISION%-Client.zip ../bin/data
