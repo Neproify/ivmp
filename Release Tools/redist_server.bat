5d5fb76fc0c335dca449fca01d9fbb2a7c19fa87
Neproify <yamasaki3214@gmail.com>
2016-02-15 16:13:16 +0100

    Release Tools update

--- Release Tools/redist_server.bat
+++ Release Tools/redist_server.bat
@@ -1,6 +1,7 @@
 @echo off
 FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-list --count HEAD`) DO (
-SET VERSION=r%%F
+SET REVISION=r%%F
 )
-7z a IVMP-%VERSION%-Server.zip ../bin/Lidgren.Network.dll
-7z a IVMP-%VERSION%-Server.zip ../bin/ivmp_server_core.exe
+SET VERSION="0.1"
+7z a IVMP-%VERSION%-%REVISION%-Server.zip ../bin/Lidgren.Network.dll
+7z a IVMP-%VERSION%-%REVISION%-Server.zip ../bin/ivmp_server_core.exe
